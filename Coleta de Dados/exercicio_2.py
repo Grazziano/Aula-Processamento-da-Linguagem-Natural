0078547c9f1a220a4d8fbc95e543b0894dcf67a8
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-06-20 21:37:05 -0300

    feat: capturando informações professores e cursos

--- Coleta de Dados/exercicio_2.py
+++ Coleta de Dados/exercicio_2.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 url = "https://institucional.ufpel.edu.br/unidades/id/443"
+
+data = {
+    'Professores': [],
+    'Cursos': [],
+}
 
 try:
     source = requests.get(url)
@@ -9,13 +15,25 @@ try:
 
     soup = BeautifulSoup(source.text, "html.parser")
 
-    # soup.find("div", {"id": "graduacao"})
-    cursos = soup.select('div#graduacao')
+    # print(soup.prettify())
 
-    print(cursos)
+    print("Cursos de Graduação")
+    print("===================")
+    for link in soup.select('div#graduacao')[0].find_all("li"):
+        # print(link.a.text)
+        data["Cursos"].append(link.a.text)
 
-    for curso in cursos:
-        name = curso.find('li').a.text
-        print(name)
+    print()
+
+    print("Professores de Graduação")
+    print("===================")
+    for link in soup.select('div#servidores')[0].find_all("td"):
+        # print(link.span.text)
+        data["Professores"].append(link.span.text)
+    
+    # Criando o DataFrame
+    # df_projetos = pd.DataFrame(data)
+    # Salvando o DataFrame em um arquivo CSV
+    # df_projetos.to_csv('/mnt/data/projetos_computacao.csv', index=False)
 except Exception as e:
     print(e)
